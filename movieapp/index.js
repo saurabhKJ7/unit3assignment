8133d832869029f1c5846658fe26f47dae64fa85
saurabhKJ7 <101393331+saurabhKJ7@users.noreply.github.com>
2022-05-27 01:01:26 +0530

    movieapp

---
+++ movieapp/index.js
@@ -0,0 +1,46 @@
+
+
+const key ="2e7333aa"
+async function getData() {
+    let enter=document.getElementById("moviename").value
+    let url = `https://omdbapi.com/?t=${enter}&apikey=${key}`;
+  
+
+    let res = await fetch(url);
+    console.log(res)
+
+    let data = await res.json();
+    console.log(data)
+    append(data);
+}
+
+function append(data) {
+  let movie = document.getElementById("movie");
+  movie.innerText = "";
+  let h3 = document.createElement("h3");
+  h3.innerText = `Title: ${data.Title}`;
+
+  let p1 = document.createElement("p");
+  p1.innerText = `Actors: ${data.Actors}`;
+  let p6 = document.createElement("p");
+  p6.innerText = `Director: ${data.Director}`;
+
+  let p2 = document.createElement("p");
+  p2.innerText = `Genre: ${data.Genre}`;
+  
+  let p3 = document.createElement("p");
+  p3.innerText =`imbd Rating: ${data.imdbRating}`;
+  let p4 = document.createElement("p");
+  p4.innerText =`Type: ${data.Type}`;
+  let p5 = document.createElement("p");
+  p5.innerText =`Plot: ${data.Plot}`;
+  let poster = document.createElement("img");
+  poster.src=data.Poster
+    let div1=document.createElement("div")
+    let div2=document.createElement("div")
+    div1.append(poster)
+    div2.append(h3,p1,p6,p2,p3,p4,p5)
+
+  movie.append(div1,div2);
+
+}
